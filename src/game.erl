d452e51a016af6d3efcc66de51b149de09a58a30
Romeo <romeo.bellon@me.com>
2016-05-18 14:13:07 +0200

    Redesigned error handling

--- src/game.erl
+++ src/game.erl
@@ -34,10 +34,10 @@ start() ->
 ready() ->
   receive
     {enter, PlayerName, No_Players} ->
-      io:format("~p has entered the game!~n~p players.~n", [PlayerName, No_Players]),
+      io:format("-->  ~p has entered the game! (~p players [Min: 2, Max: 10]).~n", [PlayerName, No_Players]),
       ready();
     {leave, PlayerName, No_Players} ->
-      io:format("~p has left the game!~n~p players.~n", [PlayerName, No_Players]),
+      io:format("-->  ~p has left the game! (~p players [Min: 2, Max: 10]).~n", [PlayerName, No_Players]),
       ready()
   end.
 
@@ -51,22 +51,25 @@ loop(S = #state{}) ->
   receive
   %% monitor the client
     {ServerPid, CtoS_Ref, {enter, PlayerName, ClientPid}} ->
-      case orddict:find(ClientPid, S#state.players) of
-        {ok, _E} ->
-          ServerPid ! {CtoS_Ref, error, "You are already in the game!"},
-          S#state.players,
-          loop(S);
-        error ->
-          StoC_Ref = erlang:monitor(process, ClientPid),
-          NewPlayers = orddict:store(ClientPid, [StoC_Ref, PlayerName], S#state.players),
-          ServerPid ! {CtoS_Ref, ok},
-          ready ! {enter, PlayerName, orddict:size(NewPlayers)},
-          loop(S#state{players = NewPlayers})
+      case orddict:size(S#state.players) + 1 =< 2 of
+        true ->
+          case orddict:find(ClientPid, S#state.players) of
+            {ok, _E} ->
+              ServerPid ! {CtoS_Ref, error_1},
+              S#state.players,
+              loop(S);
+            error ->
+              StoC_Ref = erlang:monitor(process, ClientPid),
+              NewPlayers = orddict:store(ClientPid, [StoC_Ref, PlayerName], S#state.players),
+              ServerPid ! {CtoS_Ref, ok},
+              ready ! {enter, PlayerName, orddict:size(NewPlayers)},
+              loop(S#state{players = NewPlayers})
+          end;
+        false -> ServerPid ! {CtoS_Ref, error_2}
       end;
 
   %% for debugging purposes
     {ServerPid, MsgRef, {leave, ClientPid}} ->
-      io:format("~p", [ClientPid]),
       case orddict:find(ClientPid, S#state.players) of
         {ok, Player} ->
           NewPlayers = orddict:erase(ClientPid, S#state.players),
@@ -90,7 +93,8 @@ enter(PlayerName, ClientPid) ->
   ?MODULE ! {self(), CtoS_Ref, {enter, PlayerName, ClientPid}},
   receive
     {CtoS_Ref, ok} -> io:format("You've been accepted to play!~n");
-    {CtoS_Ref, error, Reason} -> {error, Reason};
+    {CtoS_Ref, error_1} -> io:format("You've alerady entered the game!~n");
+    {CtoS_Ref, error_2} -> io:format("Maximum number of players reached!~n");
   %% if the server unexpectedly shuts down receive an error
     {'DOWN', CtoS_Ref, process, _Pid, Reason} ->
       {error, Reason}
